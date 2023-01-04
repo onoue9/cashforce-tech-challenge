37c870d218843e38a374008030e9d4326f51f78d
Filipe Onoue <filipeflom@gmail.com>
2023-01-04 12:02:25 -0300

    feat: :sparkles: iniciating frontend, vue create

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <MainContent />
+</template>
+
+<script>
+import MainContent from './components/Main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainContent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: DM Sans, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
