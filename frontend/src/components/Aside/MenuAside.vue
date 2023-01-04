2a1ad9b1f1c0b17cc881f5f64dea4339d0df2707
Filipe Onoue <filipeflom@gmail.com>
2023-01-04 12:04:26 -0300

    feat: :sparkles: implemented MenuAside component

---
+++ frontend/src/components/Aside/MenuAside.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="aside-menu">
+    <HeaderAside />
+    <ItemAside />
+  </div>
+</template>
+
+<script>
+import HeaderAside from './HeaderAside.vue';
+import ItemAside from './ItemAside.vue';
+export default {
+  name: 'MenuAside',
+  components: {
+    HeaderAside,
+    ItemAside,
+  },
+}
+</script>
+
+<style scoped>
+  .aside-menu {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px;
+  }
+</style>
