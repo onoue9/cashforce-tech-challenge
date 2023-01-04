d1b7bafb5f799f6e59a5b8b2d86510d81fa01dcf
Filipe Onoue <filipeflom@gmail.com>
2023-01-04 12:03:28 -0300

    feat: :sparkles: implemented DivAside component

---
+++ frontend/src/components/Aside/DivAside.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="aside-div">
+    <MenuAside />
+  </div>
+</template>
+
+<script>
+import MenuAside from './MenuAside.vue';
+export default {
+  name: 'DivAside',
+  components: {
+    MenuAside,
+  }
+}
+</script>
+
+<style scoped>
+.aside-div {
+    position: absolute;
+    width: 288px;
+    height: 900px;
+    left: 0px;
+    top: 0px;
+  }
+</style>
