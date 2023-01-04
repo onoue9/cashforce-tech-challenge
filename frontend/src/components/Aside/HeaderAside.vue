3ce668758c997cccb710b6f06db150db97b4e9d4
Filipe Onoue <filipeflom@gmail.com>
2023-01-04 12:04:44 -0300

    feat: :sparkles: implemented HeaderAside component

---
+++ frontend/src/components/Aside/HeaderAside.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header-menu">
+    <img src="../../assets/cashforce-title.svg" alt="cashforce title icon" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderAside',
+}
+</script>
+
+<style scoped>
+  .header-menu {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 288px;
+    height: 80px;
+  }
+</style>
