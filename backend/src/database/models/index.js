895bfc69eff03073cb565f24ffafadbdf7a2794d
Filipe Onoue <filipeflom@gmail.com>
2023-01-04 11:50:02 -0300

    feat: :sparkles: installing sequelize and iniciated

---
+++ backend/src/database/models/index.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const config = require("../config/config");
+
+const { NODE_ENV } = process.env;
+
+const sequelize = new Sequelize(
+  config[NODE_ENV] || config.development
+);
+
+module.exports = {
+  sequelize
+}
