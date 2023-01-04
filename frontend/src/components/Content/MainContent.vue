72c5375c07ca7938289f90914265d4e5280a404e
Filipe Onoue <filipeflom@gmail.com>
2023-01-04 12:05:43 -0300

    feat: :sparkles: implemented MainContent component

---
+++ frontend/src/components/Content/MainContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content">
+    <HeaderMainContent />
+    <PageContent />
+  </div>
+</template>
+
+<script>
+import HeaderMainContent from './HeaderMainContent.vue';
+import PageContent from './PageContent.vue';
+export default {
+  name: 'MainContent',
+  components: {
+    HeaderMainContent,
+    PageContent,
+  },
+}
+</script>
+
+<style scoped>
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+    gap: 10px;
+  }
+</style>
