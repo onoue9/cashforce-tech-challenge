a15768929d000257c6053c5310c3cb319e6bbbc8
Filipe Onoue <filipeflom@gmail.com>
2023-01-04 12:05:21 -0300

    feat: :sparkles: implemented HeaderContent component

---
+++ frontend/src/components/Content/HeaderContent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <div class="fame">
+      <div class="frame"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderContent',
+}
+</script>
+
+<style scoped>
+  .header {
+    width: 1272px;
+    height: 64px;
+    border-bottom: 1px solid #DFE2EB;
+  }
+
+  .fame {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 16px 46px;
+    gap: 10px;
+    background: #FFFFFF;
+    border-radius: 12px 0px 0px 0px;
+  }
+
+  .frame {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0px;
+    gap: 22px;
+  }
+</style>
