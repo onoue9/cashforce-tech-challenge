a89b6729707a5db040f068f45ca40cd8ea3bee8e
Filipe Onoue <filipeflom@gmail.com>
2023-01-04 12:02:53 -0300

    feat: :sparkles: implemented Main component

---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-div">
+    <DivAside />
+    <DivContent />
+  </div>
+</template>
+
+<script>
+import DivAside from './Aside/DivAside.vue';
+import DivContent from './Content/DivContent.vue';
+
+export default {
+  name: 'MainContent',
+  components: {
+    DivAside,
+    DivContent,
+  },
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .main-div {
+    position: absolute;
+    width: 1560px;
+    height: 900px;
+    left: 0px;
+    top: 0px;
+    background: #FBFDFE
+  }
+</style>
