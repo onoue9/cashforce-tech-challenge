e23012ccdcb5932eb71c3c2c9441d052d1ef6791
Filipe Onoue <filipeflom@gmail.com>
2023-01-04 12:03:47 -0300

    feat: :sparkles: implemented DivContent component

---
+++ frontend/src/components/Content/DivContent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="content-div">
+    <HeaderContent />
+    <MainContent />
+  </div>
+</template>
+
+<script>
+import HeaderContent from './HeaderContent.vue';
+import MainContent from './MainContent.vue';
+export default {
+  name: 'DivContent',
+  components: {
+    HeaderContent,
+    MainContent,
+  }
+}
+</script>
+
+<style scoped>
+  .content-div {
+    position: absolute;
+    width: 1272px;
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+    background: #FFFFFF;
+    box-shadow: 0px 10px 30px rgba(225, 229, 236, 0.5);
+    border-radius: 15px 0px 0px 15px;
+  }
+</style>
